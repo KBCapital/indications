76ba6b50b42b61cf681fab7587ed812490e72df6
Benjamin Shyong <ben.shyong@gmail.com>
2016-02-29 20:23:58 -0800

    add company list:

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :companies
   resources :indications
+
+  root to: 'indications#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
